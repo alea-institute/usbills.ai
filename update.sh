5c53c4c76bd8a330e5457b0af841e86541978bfe
Michael Bommarito <mike@aleainstitute.ai>
2025-01-20 14:18:22 +0000

    improve env handling for use of update.sh in cron

--- update.sh
+++ update.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-# export GOVINFO_API_KEY=
-PYTHONPATH=. poetry run python3 usbills_app/cli/update_bills.py
-PYTHONPATH=. poetry run python3 usbills_app/cli/load_bill_json.py ~/.cache/fbs/bills/
-PYTHONPATH=. poetry run python3 usbills_app/cli/update_bill_percentiles.py
-PYTHONPATH=. poetry run python3 usbills_app/cli/load_solr.py
-PYTHONPATH=. poetry run python3 usbills_app/cli/clear_cache.py
-PYTHONPATH=. poetry run python3 usbills_app/cli/generate_sitemap.py
+echo "Starting update script..."
+
+
+pipx ensurepath
+echo "Set pipx path"
+export PATH="$PATH:/home/ubuntu/.local/bin/"
+
+pushd /home/ubuntu/usbills.ai
+echo "Set working dir"
+
+export GOVINFO_API_KEY=
+export PYTHONPATH=.
+echo "Set path"
+
+poetry run python3 usbills_app/cli/update_bills.py
+poetry run python3 usbills_app/cli/load_bill_json.py ~/.cache/fbs/bills/
+poetry run python3 usbills_app/cli/update_bill_percentiles.py
+poetry run python3 usbills_app/cli/load_solr.py
+poetry run python3 usbills_app/cli/clear_cache.py
+poetry run python3 usbills_app/cli/generate_sitemap.py
+
+popd
+echo "Returned working dir"
